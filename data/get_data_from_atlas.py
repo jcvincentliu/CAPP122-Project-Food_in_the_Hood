6faf0089ec0777b2c1201791ed2e8c5c4399fc1c
Takayuki Nitta <tnitta@linux7.cs.uchicago.edu>
2022-02-20 22:45:15 -0600

    add atlas codes

--- data/get_data_from_atlas.py
+++ data/get_data_from_atlas.py
@@ -26,6 +26,8 @@ data.text
 
 """
 
+
+
 def get_atlas(url):
     data = requests.get(url)
     data_lsts = data.json()
@@ -33,5 +35,67 @@ def get_atlas(url):
     data_df = pd.DataFrame(data_lsts, columns=headers)
 
     return data_df
+
+
+def get_atlas_ver2():
+    params = {"topic":"LFA", "population":"", "period":"2019", "layer":"zip"}
+    url = "https://chicagohealthatlas.org/api/v1/data"
+    data = requests.get(url, params=params)
+    results = data.json()["results"]
+    df = append_results(results)
+
+    df.to_csv("atlas_result.csv")
+
+
+def append_results(results):
+    cols = ['key', 'period', 'data_value', 'geo_layer', 'geo_id_label', 'population', 'std_error']
+    df = pd.DataFrame(columns = cols)
+    for result in results:
+        key = result['a']
+        period = result['d']
+        data_value = result['v']
+        geo_layer = result['l']
+        geo_id_label = result['g']
+        population = result['p']
+        std_error = result['se']
+
+        df = df.append({'key': key, 'period': period, 'data_value': data_value, 'geo_layer': geo_layer,
+        'geo_id_label': geo_id_label, 'population': population, 'std_error': std_error}, ignore_index=True)
+    
+    return df
+
+        
+### Programs for getting keys  ###
+
+def get_atlas_keys():
+    base_url = "https://chicagohealthatlas.org/api/v1/topics/?offset="
+    df_topic = get_df_topic(base_url)
+
+    df_topic.to_csv("atlas_key_list.csv")
+
+
+def get_df_topic(base_url):
+    cols = ['name', 'key', 'description', 'units', 'format']
+    df_topic = pd.DataFrame(columns = cols)
+
+    for num in range(0,261,20):
+        url = base_url + str(num)
+        data = requests.get(url)
+        results = data.json()["results"]
+
+        for result in results:
+            name = result['name']
+            key = result['key']
+            desc = result['description']
+            units = result['units']
+            r_format = result['format']
+            series = pd.Series([name, key, desc, units, r_format], index=df_topic.columns)
+            df_topic = df_topic.append(series, ignore_index=True)
+
+    return df_topic
+
+
+
+
 
 
