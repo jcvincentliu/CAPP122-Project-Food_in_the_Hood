1da1f1e4f626d9342e1892b31fb506e2a0261ace
Takayuki Nitta <tnitta@linux7.cs.uchicago.edu>
2022-02-28 10:31:47 -0600

    revise_get_data_from_atlas

--- data/get_data_from_atlas.py
+++ data/get_data_from_atlas.py
@@ -1,29 +1,69 @@
 import requests
 import pandas as pd
 
-"""
 
-***sample queries***
-#Retrieve the 2014-2018 Hispanic population for a full geographic layer
-import requests
-data = requests.get("https://chicagohealthatlas.org/api/v1/data/?topic=POP&population=H&period=2014-2018&layer=zip")
-data.text
-
-import get_data_from_atlas
-url = "https://chicagohealthatlas.org/api/v1/data/?topic=POP&population=H&period=2014-2018&layer=zip"
-data = get_data_from_atlas.get_atlas(url)
-data
-"""
+topics_dic = {"Low food access":"LFA",
+"Neighborhood safety rate":"HCSNSP",
+"Per capita income":"PCI"}
 
 
-def get_atlas_ver2():
-    params = {"topic":"POP", "population":"", "period":"2014-2018", "layer":"neighborhood"}
-    url = "https://chicagohealthatlas.org/api/v1/data"
-    data = requests.get(url, params=params)
-    results = data.json()["results"]
-    df = append_results(results)
+period_dic = {"2014-2018":"2014-2018",
+              "2015":"2015",
+              "2015-2017":"2015-2017",
+              "2015-2019":"2015-2019",
+              "2016-2018":"2016-2018",
+              "2019":"2019"}
 
+
+def get_atlas_ver2(topics_dic, period_dic):
+    cols = ['key', 'period', 'data_value', 'geo_layer', 'geo_id_label', 'population', 'std_error']
+    df = pd.DataFrame(columns = cols)
+    for _, topic in topics_dic.items():
+        for _, period in period_dic.items():
+            params = {"topic":topic, "population":"", "period": period, "layer":"neighborhood"}
+            url = "https://chicagohealthatlas.org/api/v1/data"
+            data = requests.get(url, params=params)
+            results = data.json()["results"]
+            df_results = append_results(results)
+            df = pd.concat([df, df_results], axis=0)
+    df = post_processing(df)
     df.to_csv("atlas_result.csv")
+
+
+def post_processing(df):
+    key_name_dic =  create_key_name_dic()
+    for _, row in df.iterrows():
+        row_key = row['key']
+        row['key'] = key_name_dic[row_key]
+
+    df['geo_id_label'] = df['geo_id_label'].str.replace('1714000-','')
+    df = df.loc[:, ['key','geo_id_label','period','data_value']]
+    df_result = key_to_row(df)
+
+    return df_result
+
+
+def key_to_row(df):
+    name_list = []
+    for _, row in df.iterrows():
+        name_list.append(row['key'])
+    name_list = list(set(name_list))
+
+    df_result = (
+        df.groupby(['geo_id_label', 'period'])
+        .apply(lambda x: x.set_index('key').reindex(name_list)['data_value'])
+        .reset_index()
+        .rename_axis(None, axis=1))
+    
+    return df_result
+
+
+def create_key_name_dic():
+    dic = {}
+    key_name_list = pd.read_csv("atlas_key_list.csv")
+    for _, row in key_name_list.iterrows():
+        dic[row['key']] = row['name']
+    return dic
 
 
 def get_atlas_population():
@@ -110,4 +150,12 @@ def concatenating_datasets():
 
     df_merge['crime_rate'] = (df_merge['crime_num']/df_merge['population'])*10000
 
-    df_merge.to_csv("food_and_hood_data.csv")
+    df_atlas = pd.read_csv('atlas_result.csv')
+    df_atlas = df_atlas.rename(columns={'period':'year'})
+    df_atlas = df_atlas.rename(columns={'geo_id_label':'community_area'})
+    df_atlas = df_atlas.rename(columns={'data_value':'population'})
+
+    df_concatenate = pd.merge(df_atlas, df_merge ,on=['community_area','year'], how='left')
+    df_concatenate = df_concatenate.sort_values(['community_area', 'year'], ignore_index=True)
+
+    df_concatenate.to_csv("food_and_hood_data.csv", index=False)
